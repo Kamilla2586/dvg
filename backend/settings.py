4af6c609ff3c353b3202a87f9dd317b4e749e172
Kamilla Sani <kamilla@365plusun.com>
2021-06-26 11:54:13 +0200

    Add django-cors-headers to settings

--- backend/settings.py
+++ backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog",
     "graphene_django",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -124,3 +126,6 @@ STATIC_URL = "/static/"
 GRAPHENE = {
     "SCHEMA": "blog.schema.schema",
 }
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = ("http://localhost:8080",)
